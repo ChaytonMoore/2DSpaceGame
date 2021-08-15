7e3be48cabae6b91258565c0f3fc5343357c64a9
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2021-08-15 22:08:53 +0100

    Create UIElement.h

---
+++ UIElement.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+class UIElement
+{
+public: virtual ~UIElement() {}
+	int ClickRadius = 8;
+	int UISize = 64;
+	sf::Vector2i LocalPosition;
+
+	int ClickedOn();
+
+	bool IsClicked(sf::Vector2i MouseLoc);
+
+
+	void EventTick(double DeltaTime);
+
+	
+
+
+};
+

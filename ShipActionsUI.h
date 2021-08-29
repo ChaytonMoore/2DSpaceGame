e74d0c49a3c441c1c3ba91b21f172486fef82cb1
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2021-08-29 23:14:43 +0100

    Create ShipActionsUI.h

---
+++ ShipActionsUI.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "System.h"
+class ShipActionsUI
+{
+public:
+
+
+	//Buttons for the various possible things to do.
+	Button* Colonise;
+	Button* Terraform;
+
+	ShipActionsUI()
+	{
+		Colonise = new Button(sf::Vector2i(1450,100),"Colonise");
+		Terraform = new Button(sf::Vector2i(1450, 150), "Terraform");
+	}
+
+	void RenderShipActions(sf::RenderWindow* window, FleetStruct* CurrentFleet, System* CurrentSystem, Planet* CurrentPlanet, SelectEnum* select);
+	
+
+
+};
+
